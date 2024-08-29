ce2198da87e360b442f6e430c76645a90b074c54
Iftikhar Hussain <iftikharhussain2230@gmail.com>
2024-08-29 19:23:59 +0500

    Initial Commit

---
+++ VideoEditngTask/VideoEditngTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditngTaskApp.swift
+//  VideoEditngTask
+//
+//  Created by mac on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditngTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
